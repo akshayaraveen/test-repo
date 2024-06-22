3daea1395391877d3f153b764cd55ea43cdd1b80
unknown <akshaya.raveen@gmail.com>
2024-06-22 17:52:44 +0530

    counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Count = ()=>{
+    const [count,setCount] = useState(0)
     return(
         <>
-        <h2>Counter</h2>
+        <h2>{count}</h2>
+        <button onClick={()=>setCount(count+1)}>Increase</button>
+        <button onClick={()=>setCount(count-1)}>Decrease</button>
+        <button onClick={()=>setCount(0)}>Reset</button>
         </>
     )
 }
